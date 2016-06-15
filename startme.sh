267d31770bd73ea40e985af174a7cc021b90beac
El Werbitzky <werbitzky@gmail.com>
2016-06-15 02:42:43 +0200

    kill darkhttpd before start

--- startme.sh
+++ startme.sh
@@ -9,12 +9,14 @@ CERT_NAME=${CERT_NAME:-}
 if [ -z "${OVERRIDE}" ]
 then
 	mkdir -p /var/www/html
+	kill $(pgrep darkhttpd) 2> /dev/null
 	darkhttpd /var/www/html --port 80 --daemon
 	if [ -z "${SKIP_REFRESH}" ]
 	then
 		wget -qO- https://codeload.github.com/kuba/simp_le/tar.gz/master | tar xz -C /
 	fi
 	/simp_le-master/simp_le.py --default_root /var/www/html ${ARGS} && cp full.pem /certs/${CERT_NAME}_full.pem
+	kill $(pgrep darkhttpd) 2> /dev/null
 else
 	"$@"
 fi
